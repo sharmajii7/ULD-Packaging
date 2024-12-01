bc1cbae5504102609d1e249b0818a8741d37f44d
sharmajii7 <dishitsharma3741@gmail.com>
2024-12-02 00:39:17 +0530

    Rotation bug fixed

--- example8.py
+++ example8.py
@@ -11,7 +11,7 @@ This example is used to demonstrate that the algorithm does not optimize.
 # init packing function
 packer = Packer()
 #  init bin 
-box = Bin('example3', (2, 1, 3), 100,0,put_type=0)
+box = Bin('example3', (3, 2, 1), 100,0,put_type=0)
 #  add item
 # Item('item partno', (W,H,D), Weight, Packing Priority level, load bear, Upside down or not , 'item color')
 packer.addBin(box)
